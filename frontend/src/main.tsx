7dc1fff64bab5a47449b31a4b18dbb7f858ef0ad
Stefano Cipolletta <stefano.cipolletta@outlook.it>
2025-05-30 18:51:38 +0200

    Add react-router package + created sitemap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,44 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: (
+			<>
+				<header>HEADER</header>
+				<nav>NAVBAR</nav>
+				<Outlet />
+			</>
+		),
+		children: [
+			{
+				index: true,
+				Component: () => <>Home</>,
+			},
+			{
+				path: "bridge",
+				Component: () => <>Bridge</>,
+			},
+			{
+				path: "history",
+				Component: () => <>Transaction Hisotry</>,
+			},
+			{
+				path: "dividends",
+				Component: () => <>Pay dividends</>,
+			},
+			{
+				path: "fundraise",
+				Component: () => <>Fundraise</>,
+			},
+		],
+	},
+]);
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<>APP</>
+		<RouterProvider router={router} />
 	</StrictMode>
 );
