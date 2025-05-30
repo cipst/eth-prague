5881d35d610d8f8b5faa21ef45a13434cf80dde3
Stefano Cipolletta <stefano.cipolletta@outlook.it>
2025-05-31 00:45:24 +0200

    Fetch token balances using blockscout API

---
+++ frontend/src/lib/api/fetchTokenBalances.ts
@@ -0,0 +1,21 @@
+import type { TokenBalance } from "@/types/blockchain-data";
+import type { ChainData } from "./fetchChain";
+import { API_CONFIG } from "@/config/api";
+
+export const fetchTokenBalances = async (chains: ChainData[], hash: string): Promise<TokenBalance[]> => {
+	let res: TokenBalance[] = [];
+
+	const promises: Promise<Response>[] = [];
+
+	for await (const chain of chains) {
+		promises.push(fetch(`${chain.explorerUrl}${API_CONFIG.EXPLORER_API.ENDPOINTS.ADDRESS_TOKEN_BALANCES(hash)}`));
+	}
+
+	const responses = await Promise.all(promises);
+
+	const results = await Promise.all<TokenBalance>(responses.map((r) => r.json()));
+
+	res = results;
+
+	return res;
+};
