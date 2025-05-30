0d37a04dd05867cfbda933decc0705539d0b9423
Stefano Cipolletta <stefano.cipolletta@outlook.it>
2025-05-30 22:28:43 +0200

    Chains functionality

--- frontend/src/config/chain.ts
+++ frontend/src/config/chain.ts
@@ -1,3 +1,11 @@
+export type CurrencyUnit = "wei" | "gwei" | "ether";
+
+export const defaultCurrencyUnits: Record<CurrencyUnit, string> = {
+	wei: "wei",
+	gwei: "gwei",
+	ether: "ETH",
+};
+
 export const SUPPORTED_CHAINS_ID: Record<string, string> = Object.freeze({
 	"11155111": "SEPOLIA",
 });
