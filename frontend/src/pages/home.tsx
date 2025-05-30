8e7f70b6e01e8ff4bc60a4601a26d58d47e2bb2c
Stefano Cipolletta <stefano.cipolletta@outlook.it>
2025-05-30 20:26:31 +0200

    Add sidebar component into home

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,12 +1,17 @@
 import { Header } from "@/components/ui/header";
+import { Sidebar } from "@/components/ui/sidebar";
 import { Outlet } from "react-router";
 
 export const Home = () => {
 	return (
-		<main>
-			<Header />
-			<nav>NAVBAR</nav>
-			<Outlet />
-		</main>
+		<>
+			<main className="h-full flex">
+				<Header />
+				<Sidebar className="pt-20" />
+				<div className="pt-20">
+					<Outlet />
+				</div>
+			</main>
+		</>
 	);
 };
