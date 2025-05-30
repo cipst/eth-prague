28a0a9184d1c2428841b3bafbb8039ca81cd8470
Stefano Cipolletta <stefano.cipolletta@outlook.it>
2025-05-30 20:34:26 +0200

    Better style link in sidebar component

--- frontend/src/components/ui/sidebar.tsx
+++ frontend/src/components/ui/sidebar.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import { BanknoteArrowUpIcon, GalleryVerticalEndIcon, HandCoinsIcon, HomeIcon, WaypointsIcon } from "lucide-react";
 // import { Button } from "./button";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 const links = [
 	{
@@ -32,15 +32,19 @@ const links = [
 ];
 
 export const Sidebar = ({ className }: { className?: string }) => {
+	const { pathname } = useLocation();
 	return (
 		<nav className={cn("border w-xs h-full", className)}>
-			<section className="w-full h-60 bg-red-500 flex items-center justify-center">COMPANY WALLET</section>
+			<section className="w-full h-60 bg-gray-500 flex items-center justify-center">COMPANY WALLET</section>
 			<section>
 				{links.map((item) => (
 					<Link
 						to={item.url}
 						key={item.title}
-						className="hover:bg-gray-700 hover:text-white p-2 transition-all rounded-md flex items-center text-lg border border-red-500 mx-4 gap-3 font-semibold my-2">
+						className={cn(
+							"hover:bg-gray-700 hover:scale-105 hover:text-white p-2 transition-all rounded-md flex items-center text-lg border border-grey-500 mx-4 gap-3 font-semibold my-2",
+							pathname === item.url && "bg-gray-700 text-white"
+						)}>
 						<item.icon className="w-7 h-7" /> <span>{item.title}</span>
 					</Link>
 				))}
