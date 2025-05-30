b09488d5d83b6883640a33136086b8524f4f96f3
Stefano Cipolletta <stefano.cipolletta@outlook.it>
2025-05-30 22:29:23 +0200

    Prefetch chains + queryclientprovider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import { Home } from "./pages/home";
 import { MetamaskProvider } from "./lib/contexts/metamask";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { fetchChains } from "./lib/api/fetchChain";
 
 const router = createBrowserRouter([
 	{
@@ -33,10 +36,22 @@ const router = createBrowserRouter([
 	},
 ]);
 
+// Create a client
+const queryClient = new QueryClient();
+
+queryClient.prefetchQuery({
+	queryKey: ["chain-ids"],
+	queryFn: fetchChains,
+	staleTime: Infinity,
+});
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<MetamaskProvider>
-			<RouterProvider router={router} />
-		</MetamaskProvider>
+		<QueryClientProvider client={queryClient}>
+			<MetamaskProvider>
+				<RouterProvider router={router} />
+				<ReactQueryDevtools initialIsOpen={true} buttonPosition="bottom-left" />
+			</MetamaskProvider>
+		</QueryClientProvider>
 	</StrictMode>
 );
