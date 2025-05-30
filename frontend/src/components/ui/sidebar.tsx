bb3d8168b251af096463305aa526bce4e8a6ce10
Stefano Cipolletta <stefano.cipolletta@outlook.it>
2025-05-30 22:29:56 +0200

    Chain name in sidebar

--- frontend/src/components/ui/sidebar.tsx
+++ frontend/src/components/ui/sidebar.tsx
@@ -1,7 +1,12 @@
 import { cn } from "@/lib/utils";
 import { BanknoteArrowUpIcon, GalleryVerticalEndIcon, HandCoinsIcon, HomeIcon, WaypointsIcon } from "lucide-react";
-// import { Button } from "./button";
 import { Link, useLocation } from "react-router";
+import { Collapsible, CollapsibleContent } from "./collapsible";
+import { useState } from "react";
+import { useChains } from "@/hooks/useChains";
+import { Skeleton } from "./skeleton";
+import { SUPPORTED_CHAINS_ID } from "@/config/chain";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 
 const links = [
 	{
@@ -33,10 +38,38 @@ const links = [
 
 export const Sidebar = ({ className }: { className?: string }) => {
 	const { pathname } = useLocation();
+	const [isOpen, setIsOpen] = useState(false);
+	const { data: chains } = useChains();
+
 	return (
 		<nav className={cn("border w-xs h-full", className)}>
 			<section className="w-full h-60 bg-gray-500 flex items-center justify-center">COMPANY WALLET</section>
 			<section>
+				{chains ? (
+					<Collapsible open={isOpen} onOpenChange={setIsOpen} className="flex w-full flex-col gap-2 mt-4 mb-5">
+						<div className="flex items-center justify-between gap-4 px-4">
+							<h4 className="text-md font-semibold flex items-center gap-3">
+								<Avatar>
+									<AvatarImage src={chains[0].explorerLogo} />
+									<AvatarFallback>{SUPPORTED_CHAINS_ID[chains[0].chainId].slice(0, 1).toUpperCase()}</AvatarFallback>
+								</Avatar>
+								{SUPPORTED_CHAINS_ID[chains[0].chainId]}
+							</h4>
+						</div>
+						{chains.length > 1 && (
+							<CollapsibleContent className="flex flex-col gap-2">
+								{chains.slice(1).map((chain) => (
+									<div key={`tab-trigger-${chain.chainId}`} className="rounded-md border px-4 py-2 font-mono text-sm">
+										{SUPPORTED_CHAINS_ID[chain.chainId]}
+										{chain.explorerLogo}
+									</div>
+								))}
+							</CollapsibleContent>
+						)}
+					</Collapsible>
+				) : (
+					<Skeleton className="h-5 w-full" />
+				)}
 				{links.map((item) => (
 					<Link
 						to={item.url}
