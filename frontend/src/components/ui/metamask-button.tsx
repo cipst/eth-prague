10a508e62228bcf7dba8faac67ba9a97e7e6e4b1
Stefano Cipolletta <stefano.cipolletta@outlook.it>
2025-05-30 21:09:57 +0200

    Metamask button that handle the connection

---
+++ frontend/src/components/ui/metamask-button.tsx
@@ -0,0 +1,41 @@
+import { cn } from "@/lib/utils";
+import { Button } from "./button";
+import MetamaskLogo from "@/assets/metamask-logo.svg?react";
+import { useMetamask } from "@/lib/contexts/metamask";
+import { useRef } from "react";
+
+export const MetamaskButton = ({ className }: { className?: string }) => {
+	const { connectWallet, accountAddress } = useMetamask();
+	const btnRef = useRef<HTMLButtonElement>(null);
+
+	const handleConnectMetamask = async () => {
+		if (!accountAddress) {
+			await connectWallet(btnRef);
+		}
+	};
+
+	return (
+		<div className="flex items-center justify-around h-min w-2xs">
+			{!accountAddress ? (
+				<Button
+					ref={btnRef}
+					onClick={handleConnectMetamask}
+					className={cn(className, "bg-[#FF5C16] hover:bg-[#FFA680] hover:text-[#661800]", accountAddress && "hover:bg-[#FF5C16] hover:text-white")}>
+					<MetamaskLogo /> Connect Metamask
+				</Button>
+			) : (
+				// <div className="flex items-center w-full h-full gap-5">
+				<>
+					<MetamaskLogo className="w-14 h-14" />
+					<div className="flex flex-col items-start">
+						<span className="text-xl font-semibold">MetaMask</span>
+						<span className="">
+							{accountAddress.slice(0, 15)}...{accountAddress.slice(accountAddress.length - 4)}
+						</span>
+					</div>
+				</>
+				// </div>
+			)}
+		</div>
+	);
+};
