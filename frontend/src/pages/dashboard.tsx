e1364d889db48e88fa76e3265ce2244d7f7cf408
Stefano Cipolletta <stefano.cipolletta@outlook.it>
2025-05-31 12:03:19 +0200

    Change dashboard layout

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -2,8 +2,8 @@ import { BalanceSheet } from "@/components/dashboard/balance-sheet";
 
 export const Dashboard = () => {
 	return (
-		<>
+		<div className="flex flex-wrap items-center justify-center gap-5">
 			<BalanceSheet />
-		</>
+		</div>
 	);
 };
