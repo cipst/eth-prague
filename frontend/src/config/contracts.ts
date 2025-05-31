d03f72865626bb76e36e10f61b7ad0f62c080caa
Stefano Cipolletta <stefano.cipolletta@outlook.it>
2025-05-31 20:02:10 +0200

    wagmi config + contract call

---
+++ frontend/src/config/contracts.ts
@@ -0,0 +1,6 @@
+import DecomTokenJson from "../../../contracts/artifacts/contracts/Hackathon/DecomToken.sol/DecomToken.json";
+
+export const wagmiContractConfig = {
+	address: "0xB242A292F32d56241648E81B123CD9D09a130993",
+	abi: DecomTokenJson.abi,
+} as const;
--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,7 +1,17 @@
 import { BalanceSheet } from "@/components/dashboard/balance-sheet";
 import { IncomeStatement } from "@/components/dashboard/income-statement";
+import { wagmiContractConfig } from "@/config/contracts";
+import { useReadContract } from "wagmi";
 
 export const Dashboard = () => {
+	const { data: shares } = useReadContract({
+		...wagmiContractConfig,
+		functionName: "deposit",
+		args: [10],
+	});
+
+	alert(shares);
+
 	return (
 		<div className="flex flex-wrap items-start justify-center gap-5">
 			<BalanceSheet />
